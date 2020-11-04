afb432121ddc200ef0ba9e84a62bc6508acc6229
mysper <anthony.a3203@gmail.com>
2020-11-04 10:42:51 +0800

    add new command(help), fixed date message

---
+++ router/action/mirumo_bot/help.js
@@ -0,0 +1,3 @@
+module.exports = "TEST: Check whether the server is working normally\n\
+help: Get command list\
+";
--- router/action/mirumo_bot/index.js
+++ router/action/mirumo_bot/index.js
@@ -9,7 +9,14 @@ module.exports = function (req) {
                         return response = {
                             method: "sendMessage",
                             chat_id: chat.id,
-                            text: `- Date: ${Date.parse(date)}\n- Text: ${text}\n- From: @${from.username}/ ${from.id}\nServer at https://stormy-everglades-39806.herokuapp.com/bot/:token`
+                            text: `- Date: ${(new Date(date)).toString()}\n- Text: ${text}\n- From: @${from.username}/ ${from.id}\nServer at https://stormy-everglades-39806.herokuapp.com/bot/:token`
+                        };
+                    }
+                    case '/help': {
+                        return response = {
+                            method: "sendMessgae",
+                            char_id: chat.id,
+                            text: require('./help')
                         };
                     }
                     default: {
