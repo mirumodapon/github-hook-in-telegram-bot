636eb9a4e09155103cb3c456f91cbcada8e8b6e4
mysper <anthony.a3203@gmail.com>
2020-11-05 01:12:56 +0800

    update req parse

--- router/github.js
+++ router/github.js
@@ -5,34 +5,30 @@ router.post(
     '/:name',
     (req, res) => {
         console.log(req.body);
-        switch (req.body.type) {
-            //-404228386
-            case 'PushEvent': {
-                let msg = '';
-                const name = req.body.repo.name.slice(req.body.repo.name.indexOf('/') + 1, -1);
-                msg += `<b>${req.body.payload.size} commit${(req.body.payload.size > 1) ? 's' : ''} to <a href="${req.body.repo.url.replace('api.', '').replace('/repos', '')}">${name}</a></b>\n`;
-                req.body.payload.commits.forEach(element => {
-                    msg += '<del>-----------</del>\n'
-                    msg += `<a href="${element.url.replace('api.', '').replace('/repos', '').replace('commits', 'commit')}">${element.sha.slice(0, 7)}</a>: ${element.message}\n`;
-                    msg += `by ${element.author.name}\n`
-                });
-                msg += `<del>-----------</del>\nRef: ${req.body.payload.ref}`;
-                const axios = require('axios');
-                axios.post('https://api.telegram.org/bot1217070906:AAHYpyiV9zr9k2Jsxf7Ljt8MBa-cqr4n2Go/sendMessage',
-                    {
-                        method: "sendMessage",
-                        chat_id: 1157109940,
-                        parse_mode: "HTML",
-                        text: msg
-                    }
-                );
-                return res.status(204).send('No content');
-            }
-            default: {
-                return res.status(404).send('Not found');
-            }
+        const { ref, repository, pusher, commits, compare } = req.body;
+        const commits_count = commits.length;
+        var msg = `<b><a href="${compare}">${commits_count} commit${(commits_count > 1) ? 's' : ''}</a> to <a href="${repository.html_url}">${repository.name}</a></b>\n`;
+        commits.forEach(element => {
+            msg += '<del>----------</del>\n';
+            msg += `<a href="${element.url}">${element.id.slice(0, 7)}</a>: ${element.message}\nBy ${element.committer.name}`
+        });
+        msg += '<del>----------</del>\n';
+        msg += `<u>${pusher.name}</u> pushed to ${ref}\n`;
+        try {
+            const axios = require('axios');
+            axios.post('https://api.telegram.org/bot1217070906:AAHYpyiV9zr9k2Jsxf7Ljt8MBa-cqr4n2Go/sendMessage',
+                {
+                    method: "sendMessage",
+                    chat_id: 1157109940,
+                    parse_mode: "HTML",
+                    text: msg
+                }
+            );
+            return res.status(204).send('No content');
+        } catch (erro) {
+            console.error(erro.message);
+            return res.status(404).send('Not found');
         }
-
     }
 );
 
